59b1eef5dd5da74f8d9335aac976901f69a631e0
Andrea <info@andyosuna.com>
2013-03-25 09:35:01 -0700

    Admin
    Admin panel tweaks (Incomplete)

---
+++ rad-admin-tweaks/rad-admin-tweaks.php
@@ -0,0 +1,23 @@
+<?php /*
+Plugin Name: Admin Panel Tweaks
+Description: Makes the Admin Panel Better
+Author: Melissa Cabral
+Version: 1.0
+*/
+
+/**
+  * Style the Login/Register screen
+  * Since Ver. 1.0
+  **/
+function rad_login_style(){
+	?>
+	<style type="text/css">
+	body.login{
+		background-color: pink;
+	}
+	.login.form{
+
+	}
+	</style>
+	<?php
+}
